199c1c0b1a441307746dec580841e807cc9a80f5
KhadzA <alimkhadz111@gmail.com>
2025-08-29 22:17:07 +0800

    Starter

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -13,27 +13,40 @@ function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email:</label>
-        <input 
-          type="email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
-        />
-      </div>
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="max-w-[300px] w-full space-y-6 px-4">
+        <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+              LOGIN
+            </p>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Email:</label>
+                <input 
+                  type="email" 
+                  value={email} 
+                  onChange={(e) => setEmail(e.target.value)} 
+                />
+              </div>
 
-      <div>
-        <label>Password:</label>
-        <input 
-          type="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-        />
-      </div>
+              <div>
+                <label>Password:</label>
+                <input 
+                  type="password" 
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+                />
+              </div>
 
-      <button type="submit">Login</button>
-    </form>
+              <div className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+                <button type="submit">Login</button>
+              </div>
+            </form>
+          </nav>
+        </div>
+      </div>
+    </main>
   );
 }
 
