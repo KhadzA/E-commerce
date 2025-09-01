31989337a3f0d0ae29b3fafa02e8998b16512a12
KhadzA <alimkhadz111@gmail.com>
2025-09-01 23:19:19 +0800

    Starter

--- app/routes/home/layout.tsx
+++ app/routes/home/layout.tsx
@@ -4,6 +4,18 @@ import { useNavigate, Link } from "react-router";
 function HomeLayout() {
     const navigate = useNavigate();
 
+    const pages = [
+        { name: "Home", path: "/home" },
+        { name: "Products", path: "/products" },
+        { name: "Cart", path: "/cart" },
+        { name: "Profile", path: "/profile" },
+    ];
+
+    const handleNavigation = (path: string) => {
+        navigate(path);
+    };
+
+
     const handleLogout = () => {
 
         if (true) {
@@ -18,12 +30,19 @@ function HomeLayout() {
         <main className="flex items-center justify-center pt-16 pb-4">
             <div className="max-w-[300px] w-full space-y-6 px-4">
                 <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-                    <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+                    <header className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
                         <p className="leading-6 text-black dark:text-black text-center">
                             HOME LAYOUT
                         </p>
+                    </header>
+                    <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
 
-
+                        {pages.map((p) => (
+                        <div key={p.name}>
+                            <button onClick={() => handleNavigation(p.path)}>{p.name}</button>
+                            <br />
+                        </div>
+                        ))}
                         <button onClick={handleLogout}>Logout</button>
 
                     </nav>
