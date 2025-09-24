c46b3229f4098f0b89014bf91841d0967e09640f
KhadzA <alimkhadz111@gmail.com>
2025-09-24 20:41:20 +0800

    Starter

--- app/routes/admin/orders/orders.tsx
+++ app/routes/admin/orders/orders.tsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 function Orders() {
+    const [orders, setOrders] = useState<{ name: string }[]>([]);
 
-  return (
-    <main>
-        <p className="leading-6 text-black dark:text-black text-center">
-            ORDERS PAGE
-        </p>
-    </main>
-  );
+    useEffect(() => {
+      const storedOrders = JSON.parse(localStorage.getItem("orders") || "[]");
+      setOrders(storedOrders);
+    }, []);
+
+    return (
+      <div>
+        <div className="cartList">
+          {orders.map((item, index) => {
+            return (
+              <div key={index}>
+                <span className="product-images">img</span>
+                <br />
+                {item.name}
+                <br />
+              </div>
+            );
+          })}
+        </div>
+        
+        <br />
+      </div>
+    );
 }
 
 export default Orders;
