cdab33801fb5850fc3d0c50de18f1bfddc9725c1
KhadzA <alimkhadz111@gmail.com>
2025-11-17 15:45:33 +0800

    Backend

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -45,7 +45,7 @@ export const getCart = (req, res) => {
   res.json(userCart);
 };
 
-//  Add product to cartss
+//  Add product to carts
 export const addToCart = (req, res) => {
   const { userId } = req.params;
   const { productId, quantity } = req.body;
